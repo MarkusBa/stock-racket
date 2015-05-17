1f71d4ad086c87cea3c8ee7ae7f7d8a69155ee75
Markus Barthlen <Markus@doesnotexist.com>
2015-05-17 21:01:17 +0200

    init2

--- stock.rkt
+++ stock.rkt
@@ -1,8 +1,15 @@
 #lang racket
 (require net/url)
+(require net/url-structs)
 
 (define url2 "http://ichart.yahoo.com/table.csv?s=BAS.DE&a=0&b=1&c=2000&d=0&e=31&f=2010&g=w&ignore=.csv")
 
 (call/input-url (string->url url2)
                 get-pure-port
                 port->string)
+  
+(define bla (string->url url2)) 
+(url-query bla)
+(define blup (struct-copy url bla [query '((s . "BLUP")(a . "0"))]))  
+  
+
