8bf772506ff3982abbaeed1b0e875abd6ee09cec
Markus Barthlen <Markus@doesnotexist.com>
2015-05-17 21:20:31 +0200

    ..

--- stock.rkt
+++ stock.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require net/url)
 (require net/url-structs)
+(require db)
 
 (define url2 "http://ichart.yahoo.com/table.csv?s=BAS.DE&a=0&b=1&c=2000&d=0&e=31&f=2010&g=w&ignore=.csv")
 
@@ -10,6 +11,13 @@
   
 (define bla (string->url url2)) 
 (url-query bla)
-(define blup (struct-copy url bla [query '((s . "BLUP")(a . "0"))]))  
+(define blup (struct-copy url bla [query '((s . "BLUP")(a . "0"))]))
   
+(define pgc
+    (postgresql-connect #:user "markus"
+                        #:database "stock"
+                        #:password "xxxx"))
 
+(define (get-items id)
+  (query-rows pgc
+            "select * from item where idplayer = $1" id))
